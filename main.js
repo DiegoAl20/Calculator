9fafe74c90f871dc500359f5b7d566aac4766fb7
Diego Meza <alexismq16@gmail.com>
2024-02-13 11:55:37 -0700

    Add the logic for clear the screen Added a class in the HTML

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const screenResult = document.getElementById("result");
 const screenOperations = document.getElementById("operations");
+
 const btnClearAll = document.querySelector('.all-delete');
+const btnDelete = document.querySelector('.one-delete');
 
 document.addEventListener('click', (e) => {
     const clickedElement = e.target;
@@ -13,7 +15,25 @@ document.addEventListener('click', (e) => {
     screenOperations.textContent += value;
 });
 
+document.addEventListener('click', (e) => {
+    const clickedElement = e.target;
+
+    if (!clickedElement.matches(".operator")) {
+        return;
+    }
+
+    let value = clickedElement.getAttribute('value');
+    screenOperations.textContent += value;
+});
+
+// Limpia toda la pantalla
 btnClearAll.addEventListener('click', () => {
     screenOperations.textContent = ""; 
     screenResult.textContent = "";
+});
+
+// Botón para borrar el ultimo numero
+btnDelete.addEventListener('click', () => {
+    let newContent = screenOperations.textContent.slice(0, -1);
+    screenOperations.textContent = newContent;
 });
