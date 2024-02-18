0f9ffb1069d71a44d136380a2abea4020b417198
Diego Meza <alexismq16@gmail.com>
2024-02-18 14:34:58 -0700

    Equals button and divider button Add the functionality for the equals button and for the divider button I added a message error for NaN values

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const screenResult = document.getElementById("result");
+
 const screenOperations = document.getElementById("operations");
 
 const btnClearAll = document.querySelector('.all-delete');
 const btnDelete = document.querySelector('.one-delete');
+const btnEquals = document.querySelector(".equals");
+const btnDivider = document.querySelector(".divider");
 
 document.addEventListener('click', (e) => {
     const clickedElement = e.target;
@@ -36,4 +38,27 @@ btnClearAll.addEventListener('click', () => {
 btnDelete.addEventListener('click', () => {
     let newContent = screenOperations.textContent.slice(0, -1);
     screenOperations.textContent = newContent;
+});
+
+//Boton igual
+btnEquals.addEventListener('click', () => {
+    let result = eval(screenOperations.textContent);
+
+    if (isNaN(result)) {
+        screenOperations.textContent = "ERROR!";
+        return;
+    }
+    screenOperations.textContent = result;
+});
+
+// boton dividir
+btnDivider.addEventListener('click', () => {
+    let operations = +screenOperations.textContent;
+    operations /= 100;
+    
+    if (isNaN(operations)) {
+        screenOperations.textContent = "ERROR!";
+        return;
+    }
+    screenOperations.textContent = operations
 });
